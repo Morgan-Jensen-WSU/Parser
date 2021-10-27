0d1aa8b8fb3410f2f9e608651b00f4cdc6ae712b
Morgan Jensen <mbjensen822@outlook.com>
2021-10-27 16:33:05 -0600

    most of book stuff working, only works with 1 line at a time

--- Tables.cs
+++ Tables.cs
@@ -1,14 +1,29 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace parser
 {
     class TableMaker
     {
 
+        private const string FILE_PATH = "input/valid_simple.txt";
+
+        private static int ParseIndex { get; set; }
+
+        private List<char> Input = new List<char>();
+
+        private Stack<string> ParseStack = new Stack<string>();
+
         private static Dictionary<int, List<string>> Productions = new Dictionary<int, List<string>>();
         private static Dictionary<string, List<string>> First = new Dictionary<string, List<string>>();
         private static Dictionary<string, List<string>> Follow = new Dictionary<string, List<string>>();
         private static Dictionary<string, List<string>> FirstPlus = new Dictionary<string, List<string>>();
+
+        private static Dictionary<string, Dictionary<string, int>> Table = new Dictionary<string, Dictionary<string, int>>();
+
+
         private static List<string> Terminals = new List<string>
         {
             "num",
@@ -34,23 +49,171 @@ namespace parser
             "RFactor"
         };
 
+        private static List<char> StoppingChar = new List<char>
+        {
+            ' ',
+            '+',
+            '-',
+            '=',
+            '*',
+            '/',
+            '(',
+            ')',
+            '\n',
+            '\r'
+        };
+
         public TableMaker()
         {
+            TakeInput();
+            // FillProduction();
+            FillSampleProduction();
+            FillTable();
+
+            ParseIndex = 0;
+
+            if (Parse())
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        private bool Parse()
+        {
+            string word = NextWord();
+
+            ParseStack.Push("eof");
+            ParseStack.Push("Goal");
+
+            string focus = ParseStack.Peek();
+
+            while (true)
+            {
+                if (focus == "e")
+                {
+                    ParseStack.Pop();
+                    focus = ParseStack.Peek();
+                    continue;
+                }
+
+                if (focus == "eof" && word == "eof")    // end of file
+                {
+                    return true;
+                }
+                else if (Terminals.Contains(focus.ToString()) || focus == "eof")   // focus is terminal
+                {
+                    if (focus == word)
+                    {
+                        ParseStack.Pop();
+                        word = NextWord();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else    // focus is nonterminal
+                {
+
+                    if (Table[focus][word] != -1)
+                    {
+                        ParseStack.Pop();
+
+                        var production = Productions[Table[focus][word]];
+
+                        for (int i = production.Count - 1; i != -1; i--)
+                        {
+                            ParseStack.Push(production[i]);
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                focus = ParseStack.Peek();
+            }
 
         }
 
-        public void GenerateFirst()
+        private void FillSampleProduction()
         {
-            Dictionary<string, List<string>> tFirsts = new Dictionary<string, List<string>>();
-            foreach (var val in Terminals)
-            {
-                tFirsts[val] = new List<string> { val };
-            }
+            Productions[0] = new List<string> 
+            { 
+                "Expr" 
+            };
 
-            foreach (var val in NonTerminals)
-            {
-                First[val] = new List<string>();
-            }
+            Productions[1] = new List<string> 
+            { 
+                "Term",
+                "Expr'" 
+            };
+
+            Productions[2] = new List<string> 
+            { 
+                "+",
+                "Term",
+                "Expr'" 
+            };
+            
+            Productions[3] = new List<string> 
+            { 
+                "-",
+                "Term",
+                "Expr'" 
+            };
+
+            Productions[4] = new List<string> 
+            { 
+                "e" 
+            };
+
+            Productions[5] = new List<string> 
+            { 
+                "Factor",
+                "Term'" 
+            };
+
+            Productions[6] = new List<string> 
+            { 
+                "*",
+                "Factor",
+                "Term'" 
+            };
+
+            Productions[7] = new List<string> 
+            { 
+                "/",
+                "Factor",
+                "Term'"
+            };
+
+            Productions[8] = new List<string> 
+            { 
+                "e" 
+            };
+
+            Productions[9] = new List<string> 
+            { 
+                "(",
+                "Expr",
+                ")"
+            };
+
+            Productions[10] = new List<string> 
+            { 
+                "num"
+            };
+
+            Productions[11] = new List<string> 
+            { 
+                "name" 
+            };
         }
 
         private void FillProduction()
@@ -217,6 +380,127 @@ namespace parser
             };
         }
 
+        private void FillTable()
+        {
+            Table["Goal"] = new Dictionary<string, int>()
+            {
+                { "eof",   -1 },
+                { "+",     -1 },
+                { "-",     -1 },
+                { "*",     -1 },
+                { "/",     -1 },
+                { "(",      0 },
+                { ")",     -1 },
+                { "name",   0 },
+                { "num",    0 }
+            };
 
+            Table["Expr"] = new Dictionary<string, int>()
+            {
+                { "eof",   -1 },
+                { "+",     -1 },
+                { "-",     -1 },
+                { "*",     -1 },
+                { "/",     -1 },
+                { "(",      1 },
+                { ")",     -1 },
+                { "name",   1 },
+                { "num",    1 }
+            };
+
+            Table["Expr'"] = new Dictionary<string, int>()
+            {
+                { "eof",    4 },
+                { "+",      2 },
+                { "-",      3 },
+                { "*",     -1 },
+                { "/",     -1 },
+                { "(",     -1 },
+                { ")",      4 },
+                { "name",  -1 },
+                { "num",   -1 }
+            };
+
+            Table["Term"] = new Dictionary<string, int>()
+            {
+                { "eof",    -1 },
+                { "+",      -1 },
+                { "-",      -1 },
+                { "*",      -1 },
+                { "/",      -1 },
+                { "(",       5 },
+                { ")",      -1 },
+                { "name",    5 },
+                { "num",     5 }
+            };
+
+            Table["Term'"] = new Dictionary<string, int>()
+            {
+                { "eof",     8 },
+                { "+",       8 },
+                { "-",       8 },
+                { "*",       6 },
+                { "/",       7 },
+                { "(",      -1 },
+                { ")",       8 },
+                { "name",   -1 },
+                { "num",    -1 }
+            };
+
+            Table["Factor"] = new Dictionary<string, int>()
+            {
+                { "eof",    -1 },
+                { "+",      -1 },
+                { "-",      -1 },
+                { "*",      -1 },
+                { "/",      -1 },
+                { "(",       9 },
+                { ")",      -1 },
+                { "name",   11 },
+                { "num",    10 }
+            };
+        }
+
+        private void TakeInput()
+        {
+            using (StreamReader reader = new StreamReader(FILE_PATH))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    Input.Add((char)reader.Read());
+                }
+            }
+        }
+
+        private string NextWord()
+        {
+            if (ParseIndex >= Input.Count) return "eof";
+
+            StringBuilder builder = new StringBuilder();
+
+            do
+            {
+                builder.Append(Input[ParseIndex]);
+                ParseIndex++;
+            }
+            while (!StoppingChar.Contains(Input[ParseIndex]));
+            ParseIndex++;
+
+            string builtString = builder.ToString();
+
+            int temp = 0;
+            if (Int32.TryParse(builtString, out temp))
+            {
+                return "num";
+            }
+            else if (!Terminals.Contains(builtString))
+            {
+                return "name";
+            }
+            else
+            {
+                return builtString;
+            }
+        }
     }
 }
